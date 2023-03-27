bd3010a8f363eda96d27e600bbb26997c96aa3f6
alexandraktang <alexandratang09@gmail.com>
2023-03-27 16:58:33 -0400

    added matching functionality to fruit sorting game
    added OnClick() scripts to prefabs & logic to scripts

--- Assets/Scripts/Minigames/FruitSortingGameController.cs
+++ Assets/Scripts/Minigames/FruitSortingGameController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,14 +9,15 @@ public class FruitSortingGameController : MonoBehaviour
 {
     private int livesLeft = 5; //start with 5 lives at the beginning
     public GameObject livesObject;
-    List<Image> livesList = new List<Image>();
-    List<string> fruitNameList = new List<string>() {"apple","apple","banana","banana","grapes","grapes","strawberry","strawberry","watermelon","watermelon","orange","orange","pineapple","pineapple","peach","peach"};
-    List<FruitCard> cardList = new List<FruitCard>();
-    List<FruitCard> flippedCards = new List<FruitCard>();
+    private List<Image> livesList = new List<Image>();
+
+    private List<string> fruitNameList = new List<string>() {"apple","apple","banana","banana","grapes","grapes","strawberry","strawberry","watermelon","watermelon","orange","orange","pineapple","pineapple","peach","peach"};
+    private List<FruitCard> cardList = new List<FruitCard>();
+    private List<FruitCard> flippedCardsList = new List<FruitCard>();
 
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         // Get list of lives
         foreach (Image item in livesObject.GetComponentsInChildren<Image>() ) {
@@ -27,14 +29,14 @@ public class FruitSortingGameController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         
     }
 
-    public void RandomizeCards() 
+    private void RandomizeCards() 
     {
-        for (int i = 0; i < 16; i++)
+        for (int i = 0; i < 16; i++) // 16 cards
         {
             FruitCard fc = ScriptableObject.CreateInstance<FruitCard>();
             string fruitType = GenerateFruitType();
@@ -47,41 +49,72 @@ public class FruitSortingGameController : MonoBehaviour
         }
     }
 
-    public string GenerateFruitType()
+    private string GenerateFruitType()
     {
-        int index = Random.Range(0, fruitNameList.Count);
+        int index = UnityEngine.Random.Range(0, fruitNameList.Count);
         string fruitName = fruitNameList.ElementAt(index);
         fruitNameList.RemoveAt(index); // remove this fruit from the list
         return fruitName;
     }
 
-    
-    //Just for debugging purposes right now
-    public void OnCardClick() 
+    public void OnCardClick(Button button) 
     {
-        Debug.Log("Card clicked!");
+        string buttonString = button.name;
+        int buttonInt = (int) Int64.Parse(buttonString); // converts button name string to int
+        Debug.Log(buttonInt + " was clicked!");
+
+        FruitCard flippedCard = ScriptableObject.CreateInstance<FruitCard>();
+        flippedCard = cardList[buttonInt];
+        flippedCardsList.Add(flippedCard);
+        foreach (FruitCard item in flippedCardsList)
+        {
+            Debug.Log(item.FruitCardToString());
+        }
+
         // Flip animation
-        // if is first card, wait for second card to be flipped
-        // if is second card, don't allow more clicking & check if match
+        if (flippedCardsList.Count == 2) // if it is the second card
+        {
+            // don't allow clicking other buttons (low priority)
+            CheckMatch();
+        }
+        else if (flippedCardsList.Count > 2)
+        {
+            Debug.Log("ERROR: max two cards can be flipped at a time");
+        }
+        
     }
 
-    public bool CheckMatch()
+    private bool CheckMatch()
     {
         // if fruit types match, deactivate buttons
-        // else, decrease lives and flip cards back over
+        FruitCard firstCard = flippedCardsList[0];
+        FruitCard secondCard = flippedCardsList[1];
 
-        return true; // temporary return statement so compiler doesn't yell at me
+        if (firstCard.fruitType == secondCard.fruitType)
+        {
+            Debug.Log("Match!");
+            flippedCardsList.Clear();
+            return true;
+        }
+        else // else, decrease lives and flip cards back over
+        {
+            Debug.Log("Not a match!");
+            flippedCardsList.Clear();
+            DecreaseLives();
+            return false;
+        }
     }
 
 
     // If there isn't a match, decrease the number of lives
-    public void DecreaseLives()
+    private void DecreaseLives()
     {
+        Color lostLife = new Color(132,132,132);
         if (livesLeft > 0 && livesList.Count > 0)
         {
             livesLeft = livesLeft - 1;
+            livesList.Last().color = lostLife;
             livesList.Remove(livesList.Last());
-            // change monkey tint!
             // might need to get a list of the monkey lives (last in, first out)
         }
         else

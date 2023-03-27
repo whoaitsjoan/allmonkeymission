ff1056efbdb4c80a13c368b65fbf74fcda34dac5
alexandraktang <alexandratang09@gmail.com>
2023-03-27 02:41:57 -0400

    added functions to fruit sorting game script
    + missed meta file

--- Assets/Scripts/Minigames/FruitSortingGameController.cs
+++ Assets/Scripts/Minigames/FruitSortingGameController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class FruitSortingGameController : MonoBehaviour
 {
+    private int livesLeft = 5; //start with 5 lives at the beginning
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +17,37 @@ public class FruitSortingGameController : MonoBehaviour
     void Update()
     {
         
+    }
+
+    
+    //Just for debugging purposes right now
+    public void OnCardClick() 
+    {
+        Debug.Log("Card clicked!");
+    }
+
+    public bool CheckMatch()
+    {
+        // if fruit types match, deactivate buttons
+        // else, decrease lives and flip cards back over
+
+        return true; // temporary return statement so compiler doesn't yell at me
+    }
+
+
+    // If there isn't a match, decrease the number of lives
+    public void DecreaseLives()
+    {
+        if (livesLeft > 0)
+        {
+            livesLeft = livesLeft - 1;
+            
+            // change monkey tint!
+            // might need to get a list of the monkey lives (last in, first out)
+        }
+        else
+        {
+            Debug.Log("ERROR: No lives left!");
+        }
     }
 }

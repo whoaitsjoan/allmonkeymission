ca54e80d099f0cade2f8ae28336ac98321479e32
alexandraktang <alexandratang09@gmail.com>
2023-03-29 17:12:57 -0400

    updated flipcard animation functionality
    integrated with animationevents

--- Assets/Scripts/Animation/FlipCardAnimatorController.cs
+++ Assets/Scripts/Animation/FlipCardAnimatorController.cs
@@ -5,8 +5,15 @@ using UnityEngine.UI;
 
 public class FlipCardAnimatorController : MonoBehaviour
 {
-    Animator anim;
-    private Button button;
+    private Animator anim;
+    public FruitSortingGameController fruitSortingGameController;
+    
+    /* 
+    *  keep references to fruitCard flipped & buttonImage for CallSetFruitSprite
+    *  because AnimationEvents don't allow functions with params other than int, float, string
+    */
+    private FruitCard fc;
+    private Image buttonImg;
 
     // Start is called before the first frame update
     private void Start()
@@ -14,14 +21,21 @@ public class FlipCardAnimatorController : MonoBehaviour
         anim = GetComponent<Animator>();
     }
 
-    public void SetFlippingTrue() 
+    public void SetFlippingTrue(FruitCard fruitCard, Image buttonImage) 
     {
         anim.SetBool("flipping", true);
         Debug.Log("Flipping set true!");
+        fc = fruitCard;
+        buttonImg = buttonImage;
     }
 
     public void SetFlippingFalse()
     {
         anim.SetBool("flipping", false);
+    }
+
+    public void CallSetFruitSprite() // created to call in AnimationEvent
+    {
+        fruitSortingGameController.SetFruitSprite(fc, buttonImg);
     }
 }

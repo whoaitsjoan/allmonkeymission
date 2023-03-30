993bbc3e732f0f48e925138bfa781b444d6a7dc7
whoaitsjoan <whoaitsjoan@protonmail.com>
2023-03-30 01:10:29 -0400

    more popup prep
    added itween plugin for popup window animation, more setup in popup game controller

--- Assets/Scripts/Minigames/PopupGameController.cs
+++ Assets/Scripts/Minigames/PopupGameController.cs
@@ -9,14 +9,28 @@ public class PopupGameController : MonoBehaviour
 {
     //grabbing game objects needed for main minigame function first
     private int livesLeft = 5; 
-   // public GameObject livesObject;
+    public GameObject livesObject;
     private List<Image> livesList = new List<Image>();
-    private List<PopupWindow> windowList = new List<PopupWindow>();
+    [SerializeField]private List<GameObject> windowList = new List<GameObject>();
+    [Range (1,10)]
+    [SerializeField] private int numberOfWindows;
 
     //now grabbing items for start of minigame/ongoing timer
     [SerializeField]private TextMeshProUGUI startText;
     private float startTimer = 3.0f;
+    
 
+
+    void Awake() 
+    {
+    foreach (Image item in livesObject.GetComponentsInChildren<Image>()) 
+        {
+            livesList.Add(item);
+        }
+
+        StartCoroutine(CreatePopups());
+
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -27,8 +41,16 @@ public class PopupGameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //starts a countdown in the start text
         startTimer -= Time.deltaTime;
         startText.text = (startTimer).ToString("0");
+        if (startTimer < 0)
+        { startText.enabled = false; }
 
+    }
+
+    IEnumerator CreatePopups()
+    {
+        yield return new WaitForSeconds(startTimer);
     }
 }
